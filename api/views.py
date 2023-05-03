dbe374d677234884c97bd7fdd69c9685eff937d7
anualias98 <122264818+anualias98@users.noreply.github.com>
2023-05-03 13:06:24 +0530

    second commit

--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from rest_framework import generics, permissions
 from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 from rest_framework.response import Response
 from .serializers import ChocoSerializer
-from .models import  Chocolate
+from .models import Chocolate
+
+
 # Create your views here.
 
 
@@ -13,10 +15,11 @@ class ListChocolate(generics.ListCreateAPIView):
     queryset = Chocolate.objects.all()
     serializer_class = ChocoSerializer
 
+    def list(self, request):
+        queryset = self.get_queryset()
+        return Response({'object_list': queryset})
 
-    def list(self,request):
-        queryset=self.get_queryset()
-        return Response({'object_list':queryset})
+
 class DetailChoco(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = [permissions.IsAuthenticated]
     renderer_classes = [TemplateHTMLRenderer]
@@ -24,9 +27,11 @@ class DetailChoco(generics.RetrieveUpdateDestroyAPIView):
     queryset = Chocolate.objects.all()
     serializer_class = ChocoSerializer
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         queryset = self.get_object()
-        return Response({'object':queryset})
+        return Response({'object': queryset})
+
+
 class ChocoCheckoutView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     renderer_classes = [TemplateHTMLRenderer]
@@ -34,12 +39,10 @@ class ChocoCheckoutView(generics.ListAPIView):
     queryset = Chocolate.objects.all()
     serializer_class = ChocoSerializer
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         queryset = self.get_object()
-        return Response({'object':queryset})
+        return Response({'object': queryset})
+
+
 def home(request):
-    return render(request,'home.html')
-
-
-
-
+    return render(request, 'home.html')
